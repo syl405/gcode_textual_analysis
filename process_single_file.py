90008c32fae74defe337f7b5aab7eeb2b07b7221
Shien Yang Lee <shienyanglee@shien.tech>
2017-05-25 16:01:45 -0400

    Add logic to compute naive estimate of print time.

--- process_single_file.py
+++ process_single_file.py
@@ -22,18 +22,21 @@ def process_single_file(fs):
 		raise NameError('G-code file is missing NVBOTS-generated header')
 
 	#Raw textual features
-	num_lines_gcode = 0
-	num_lines_comment = 0
-	num_purges = 0
-	num_lin_moves = 0
-	total_dist_print = 0
-	num_temp_change = 0
-	num_retract = 0
-	num_unretract = 0
-	length_extruded = 0
-	num_temp_check = 0
-	num_fan_on = 0
-	num_fan_off = 0
+	output = {'num_lines_gcode':0, #ok
+			  'num_lines_comment': 0, #ok
+			  'num_lines_move': 0, #ok
+			  'total_dist_move': 0, #ok
+			  'total_dist_print': 0, #ok
+			  'num_temp_changes': 0, #ok
+			  'num_retract': 0, #ok
+			  'num_unretract': 0, #ok
+			  'total_length_extruded': 0, #ok
+			  'num_temp_checks': 0, #ok
+			  'num_fan_on': 0, #ok
+			  'num_fan_off': 0, #ok
+			  'num_purges': 0, #ok
+			  'total_dwell_time': 0, #ok
+			  'naive_print_time': 0}
 
 	#Raft and print parameters
 	seam_position = 0
@@ -62,50 +65,88 @@ def process_single_file(fs):
 	in_header = 0
 	in_footer = 0
 
-	#Compiled regex patterns
-	move_pattern = ';((?:X|Y|E|F)\d+\.?\d*)'
+	#Variables to store current axis position
+	motion_params ={'X': 0, #current x position in mm
+					'Y': 0, #current y position in mm
+					'Z': 0, #current z position in mm
+					'F': 0, #current feedrate in mm/s
+					'A': 0, #current acceleration in mm/s^2
+					'T': 0} #current temperature in degsC
+
+	#Pre-compiled regex patterns
+	move_pattern = '((?:X|Y|E|F)\d+\.?\d*)'
 	move_regex = re.compile(move_pattern)
+	temp_pattern = 'S(\d+\.?\d*)'
+	temp_regex = re.compile(temp_pattern)
+	dwell_pattern = 'P(\d+\.?\d*)'
+	dwell_regex = re.compile(dwell_pattern)
 
 	#Options dictionary for parsing X, Y, E, F numbers
-	xyef_opts = {'X': lambda axis_str:float(axis_str(1:-1)),
-				 'Y': lambda axis_str:float(axis_str(1:-1)),
-				 'E': lambda axis_str:float(axis_str(1:-1)),
-				 'F': lambda axis_str:float(axis_str(1:-1))}
+	xyef_opts = {'X': lambda axis_str:float(axis_str[1:-1]),
+				 'Y': lambda axis_str:float(axis_str[1:-1]),
+				 'Z': lambda axis_str:float(axis_str[1:-1]),
+				 'E': lambda axis_str:float(axis_str[1:-1]),
+				 'F': lambda axis_str:float(axis_str[1:-1])}
 
 	#Read in header
 	cur_line = fs.readline() #read first line
 	while cur_line.startswith(';'): #exit when past initial block of comments
-		num_lines_gcode += 1
-		num_lines_comment += 1
+		output['num_lines_gcode'] += 1
+		output['num_lines_comment'] += 1
 		cur_line = fs.readline() #read next line
 
 	#Iterate through remainder of lines
 	while cur_line: #Exit loop when cur_line is empty, i.e. EOF
 		if cur_line.startswith(';'):
-			num_lines_comment += 1
+			output['num_lines_comment'] += 1
 			if cur_line == '; Begin move/purge/wipe gcode': #detect start of purge routine
-				num_purges += 1
+				output['num_purges'] += 1
 				in_purge = 1
 			elif cur_line == '; End move/purge/wipe gcode':
 				in_purge = 0
-		elif cur_line.startswith('G0 ') or cur_line.startswith('G1 '):
+		elif cur_line.startswith('G0 ') or cur_line.startswith('G1 '): #move lines
+			deltas ={'X': 0, #x motion this line in mm
+					 'Y': 0, #y motion this line in mm
+					 'Z': 0} #z motion this line in mm
 			match_lists = move_regex.findall(cur_line)
 			for i in match_lists:
+				cur_axis = match_lists[i][0] #first character in string
+				cur_axis_dest = float(match_lists[i][1:])
+				if cur_axis in ('X','Y','Z'):
+					deltas[cur_axis] = cur_axis_dest - motion_params[cur_axis] #calculate relative motion
+				motion_params[cur_axis] = cur_axis_dest #update machine position
+			cur_move_dist = ((deltas['X'])^2+(deltas['Y'])^2)^0.5 #pythagorean theorem
+			output['total_dist_move'] += cur_move_dist
+			if motion_params['E']:
+				output['total_dist_print'] += cur_move_dist
+			output['total_length_extruded'] += motion_params['E']
+			output['num_lines_move'] += 1
 
-		elif cur_line.startswith('G1 '):
+			#calculate naive print time
+			if cur_move_dist > motion_params['F']^2 / (2*motion_params['A']): #trapezoidal profile
+				print_time_this_move = ((2*motion_params['F'])/motion_params['A'])+((cur_move_dist-((motion_params['F'])^0.5/(motion_params['A'])))/(motion_params['F']))
+			else: #triangular profile
+				#cusp_speed = (motion_params['A']*cur_move_dist)^0.5 (for reference only, close form solution)
+				print_time_this_move = (2*cur_move_dist)/((motion_params['A']*cur_move_dist)^0.5)
 
-
-		elif cur_line.startswith('G10'):
-			num_retract += 1
-		elif cur_line.startswith('G11'):
-			num_unretract += 1
-
-
-			
-
+		elif cur_line.startswith('M105 '): #temp check lines
+			output['num_temp_checks'] += 1
+		elif cur_line.startswith('M104 '): #temp set lines
+			output['num_temp_changes'] += 1
+			motion_params['T'] = temp_regex.search(cur_line).group(1)
+		elif cur_line.startswith('G10 '): #retract lines
+			output['num_retract'] += 1
+		elif cur_line.startswith('G11 '): #unretract lines
+			output['num_unretract'] += 1
+		elif cur_line.startswith('G4 '): #waiting lines
+			output['total_dwell_time'] += dwell_regex.search(cur_line).group(1)
+		elif cur_line.startswith('M107 '): #fan off lines
+			output['fan_off'] += 1
+		elif cur_line.startswith('M106 '): #unretract lines
+			output['fan_on'] += 1
 				
 
-		num_lines_gcode += 1
+		output['num_lines_gcode'] += 1
 		cur_line = fs.readline()
 
-		
+	return outputs
